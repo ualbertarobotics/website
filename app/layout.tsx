bc988c843af7879d398301f528d8dc729baa2837
Matvey <matvey.okoneshnikov@gmail.com>
2025-01-20 19:43:36 -0700

    Add openGraph metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,28 @@ export const metadata: Metadata = {
     title: "UofA Robotics Association",
     description:
         "The University of Alberta Robotics Association is a dynamic group of innovators passionate about advancing robotics technology.",
+    openGraph: {
+        title: "University of Alberta Robotics Association",
+        description:
+            "Join the University of Alberta Robotics Association to explore innovation, creativity, and cutting-edge robotics technology.",
+        url: "https://uara.ca",
+        images: [
+            {
+                url: "/Logo.svg",
+                width: 1200,
+                height: 1200,
+                alt: "University of Alberta Robotics Association Logo",
+            },
+        ],
+        type: "website",
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: "UofA Robotics Association",
+        description:
+            "Join the University of Alberta Robotics Association to explore innovation, creativity, and cutting-edge robotics technology.",
+        images: ["/Logo.svg"],
+    }
 };
 
 export default function RootLayout({
